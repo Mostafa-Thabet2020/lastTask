52325c54bd970395db53cd7c770b1a6fa9081d4e
Mostafa-Thabet2020 <62522809+Mostafa-Thabet2020@users.noreply.github.com>
2021-11-13 22:10:31 +0200

    Add project files.

---
+++ lastTask/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+User Account = new User(); // instance from class
+
+Console.WriteLine("Enter your first name");
+Account.firstName = Console.ReadLine();
+
+Console.WriteLine("Enter your last name");
+Account.lastName=Console.ReadLine();
+
+Console.WriteLine("Enter your email");
+Account.email = Console.ReadLine();
+
+Console.WriteLine("Enter your password");
+Account.password=Console.ReadLine();
+
+Console.WriteLine("Enter you confirm password");
+Account.confirmPassword=Console.ReadLine();
+
+Console.WriteLine("Enter your Monthely solory");
+Account.mounthSalary=Convert.ToDouble( Console.ReadLine());
+
+Console.WriteLine("Enter your birthday");
+Account.birthday = Convert.ToDateTime(Console.ReadLine());
+
+Console.WriteLine("Enter your National id");
+Account.nationalId = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your phone number");
+Account.phoneNumber = Console.ReadLine();
+
+if (Account.IsMatch())
+{
+    if (Account.IsUserAdult())
+    {
+        Console.WriteLine("User is allowed to use this app");
+        if (Account.NationalIDValidation())
+        {
+            Console.WriteLine(Account.GetFullName());
+            Console.WriteLine(Account.YearlySalary());        
+        }
+        else
+        {
+            Console.WriteLine("national id is not correct");
+        }
+    }
+    else
+    {
+        Console.WriteLine("User is not allowed to use this app");
+        Console.ReadLine();
+    }
+}
+else
+{
+    Console.WriteLine("Password not match");
+}
+
+
---
+++ lastTask/User.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+//string length
+//confirm password
+
+public class User
+{
+    public string firstName;
+    public string lastName;
+    public string email;
+    public string password;
+    public string confirmPassword;
+    public DateTime birthday;
+    public double mounthSalary;
+    public string phoneNumber;
+    public int nationalId;
+
+    private int adultAge = 18;
+
+    private DateTime CurrentTime = DateTime.Now;
+    private int ColculateAge()
+    {
+        return CurrentTime.Year - birthday.Year;
+    }
+    public bool IsUserAdult()
+    {
+
+        if (ColculateAge() >= adultAge)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool IsMatch()
+    {
+        if (password == confirmPassword)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool NationalIDValidation()
+    {
+        string NationalIdString = Convert.ToString(nationalId);
+        if (NationalIdString.Length == 5)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public string GetFullName()
+    {
+
+        //return "full name is " + firstName + " " + lastName;
+        return $"full name is {firstName} {lastName}"; // best way
+        //return string.Concat(firstName, lastName);
+    }
+    public string YearlySalary()
+    {
+        //double yearly = mounthSalary * 12;
+        //string text = "Yearly salary is ";
+        //string lastText = text + Convert.ToString(yearly);
+        //return lastText;
+        
+        return string.Concat("Yearly salary is ", Convert.ToString( mounthSalary * 12));
+    }
+}
